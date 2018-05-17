c768ec61c0f0dcd6c4b46d28ecdcb4859bd790f0
Angelin01 <angeloborsoiross@hotmail.com>
2018-05-16 22:50:07 -0300

    Terminada função de init da UART
    Pequenos ajustes ao formato do código

--- lab04/uart.c
+++ lab04/uart.c
@@ -1,15 +1,27 @@
 #include "uart.h"
 
 void uart_init() {
-	*RCGCUART |= 0x1; // 1 - Habilita o UART0 no registrador de controle de modulos
-	*RCGCGPIO |= 0x1; // 2 - Habilita clock para os modulos de UART
-	*GPIOAFSELA |= 0x3; // 3 - Troca os pinos 33 e 34 (PA0 e PA1) para controle de periferico
-	// 4 - Nao eh necessario se preocupar com slew rate ou corrente nesse caso
-	*GPIOPCTLA |= 0x11; /* Pouco mais complexo. Ver tabela 26.5 na pagina 1808
-	                       Seleciona o PMC1, de acordo com a tabela. Este eh o bit LSB.
-	                       Entao habilita o PA com outro bit */
+	accessReg(RCGCUART) |= (1<<0); // Habilita o UART0 no registrador de controle de modulos
+	accessReg(RCGCGPIO) |= (1<<0); // Habilita clock para os modulos de UART
+	accessReg(GPIOAFSELA) |= (1<<0) | (1<<1); // Troca os pinos 33 e 34 (PA0 e PA1) para controle de periferico
+	// Nao eh necessario se preocupar com slew rate ou corrente nesse caso
+	accessReg(GPIOPCTLA) |= (1<<0) | (1<<4); /* Pouco mais complexo. Ver tabela 26.5 na pagina 1808
+	                                            Seleciona coluna 1 no PCM0 e PCM1 para PA0 e PA1 */
 						 
 /**** AVISO: O REGISTRADOR A SEGUIR TROCA PINOS DO ESTADO DE TRI-STATE PARA INPUT OUTPUT DIGITAL ****/
-/**** POR SEGURANCA, ISSO FICARA DESABILITADO ATE QUE SE PROVE NECESSARIO ****/
-//	*GPIOADEN | = 0x3; // Habilita sinais digitais nos pinos do GPIO		
+//	accessReg(GPIOADEN) |= (1<<0) | (1<<1); // Habilita sinais digitais nos pinos do GPIO		
+	
+	accessReg(UART0CTL) &= 0xfffe; // Desabilita UART (bit 0) antes de aplicar configuracoes
+	
+	// Proximos dois registradores recebem os valores de divisor de baud rate inteiro e fracinado
+	/* BRD = clock / (divisor * baudRate)
+	   BRD = 120000000 / (16 * 115200) = 65,10416666666667 = 65
+	   FRAC = int(0.10416666666667 * 64 + 0.5) = int(7,16667) = 7 */
+	accessReg(UART0IBRD) = 65;
+	accessReg(UART0FBRD) = 7;
+	
+	accessReg(UART0LCRH) = 0x60; // = 0b01100000 Configura o serial para 8 data bits, 1 stop bit sem paridade
+	accessReg(UART0CC) = 0x0; // Configura para usar o clock normal do sistema (igual a reset)
+	
+	accessReg(UART0CTL) |= (1<<0); // Reabilita o UART
 }
--- lab04/uart.h
+++ lab04/uart.h
@@ -12,12 +12,26 @@
 #ifndef __UART_LAB4_H__
 #define __UART_LAB4_H__
 
-#define RCGCUART (volatile uint32_t*) 0x400FE618 // Registrador de controle dos modulos UART
-#define RCGCGPIO (volatile uint32_t*) 0x400FE608 // Registrador de controle de clock de modulos de I/O
-#define GPIOAFSELA (volatile uint32_t*) 0x40058420 // Registrador que controla se pinos são I/O generico ou usados por perifericos para porta A
-#define GPIOPCTLA (volatile uint32_t*) 0x4005852C // Registrador usado em conjunto com GPIOAFSELA para selecionar o tipo de sinal para cada pino
-#define GPIOADEN (volatile uint32_t*) 0x4005851C // Registrador que habilita e desabilita sinais digitais
+// Para facilidade de uso dos registradores
+#define accessReg(x) \
+        (*((volatile uint32_t *)(x)))
 
-bool uart_init();
+// Registradores de configuracao
+#define RCGCUART   0x400FE618 // Registrador de controle dos modulos UART
+#define RCGCGPIO   0x400FE608 // Registrador de controle de clock de modulos de I/O
+#define GPIOAFSELA 0x40058420 // Registrador que controla se pinos são I/O generico ou usados por perifericos para porta A
+#define GPIOPCTLA  0x4005852C // Registrador usado em conjunto com GPIOAFSELA para selecionar o tipo de sinal para cada pino
+#define GPIOADEN   0x4005851C // Registrador que habilita e desabilita sinais digitais
+#define UART0CTL   0x4000C030 // Registrador de controle da UART
+#define UART0IBRD  0x4000C024 // Registrador que guarda o valor inteiro do divisor de baudrate
+#define UART0FBRD  0x4000C028 // Registrador que guarda o valor de fracao do divisor de baudrate
+#define UART0LCRH  0x4000C02C // Registrador de configuracao da serial (data bits, paridade, stop bit, etc)
+#define UART0CC    0x4000CFC8 // Registrador de controle de clock source da UART
+
+// Registradores de flag e transmissao
+#define UART0FR    0x4000C018 // Registrador de flags
+#define UART0DR    0x4000C000 // Registrador de dados
+
+void uart_init();
 
 #endif
