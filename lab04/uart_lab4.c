3ef8e124ee15299c61b3d083b28cbfc7e0af8a2d
Angelin01 <angeloborsoiross@hotmail.com>
2018-05-17 15:17:41 -0300

    Seria bom se eu desse add nos arquivos

---
+++ lab04/uart_lab4.c
@@ -0,0 +1,98 @@
+#include "uart_lab4.h"
+
+void uart_init(uint32_t baudRate, uint32_t dataConfig) {
+	accessReg(RCGCUART) |= (1<<0); // Habilita o UART0 no registrador de controle de modulos
+	accessReg(RCGCGPIO) |= (1<<0); // Habilita clock para os modulos de UART
+	accessReg(GPIOAFSELA) |= (1<<0) | (1<<1); // Troca os pinos 33 e 34 (PA0 e PA1) para controle de periferico
+	// Nao eh necessario se preocupar com slew rate ou corrente nesse caso
+	accessReg(GPIOPCTLA) |= (1<<0) | (1<<4); /* Pouco mais complexo. Ver tabela 26.5 na pagina 1808
+	                                            Seleciona coluna 1 no PCM0 e PCM1 para PA0 e PA1 */
+						 
+/**** AVISO: O REGISTRADOR A SEGUIR TROCA PINOS DO ESTADO DE TRI-STATE PARA INPUT OUTPUT DIGITAL ****/
+	accessReg(GPIOADEN) |= (1<<0) | (1<<1); // Habilita sinais digitais nos pinos do GPIO		
+	
+	accessReg(UART0CTL) &= 0xfffe; // Desabilita UART (bit 0) antes de aplicar configuracoes
+	
+	// Proximos dois registradores recebem os valores de divisor de baud rate inteiro e fracinado
+	/*
+	Valores calculados sao tabelados de acordo com as seguintes formulas:
+		IBRD = INT(CLOCK / (DIVISORdeCLOCK * BAUDRATE))
+		FBRD = INT(DECIMAL(IBRD) * 64 + 0.5)
+	*/
+	switch(baudRate) {
+		case BAUD_75:
+			accessReg(UART0IBRD) = 100000;
+			accessReg(UART0FBRD) = 0;
+			break;
+		
+		case BAUD_110:
+			accessReg(UART0IBRD) = 68181;
+			accessReg(UART0FBRD) = 52;
+			break;
+		
+		case BAUD_300:
+			accessReg(UART0IBRD) = 25000;
+			accessReg(UART0FBRD) = 0;
+			break;
+		
+		case BAUD_1200:
+			accessReg(UART0IBRD) = 6250;
+			accessReg(UART0FBRD) = 0;
+			break;
+		
+		case BAUD_2400:
+			accessReg(UART0IBRD) = 3125;
+			accessReg(UART0FBRD) = 0;
+			break;
+		
+		case BAUD_4800:
+			accessReg(UART0IBRD) = 5;
+			accessReg(UART0FBRD) = 1562;
+			break;
+		case BAUD_9600:
+			accessReg(UART0IBRD) = 781;
+			accessReg(UART0FBRD) = 16;
+			break;
+		
+		case BAUD_19200:
+			accessReg(UART0IBRD) = 390;
+			accessReg(UART0FBRD) = 40;
+			break;
+		
+		case BAUD_38400:
+			accessReg(UART0IBRD) = 195;
+			accessReg(UART0FBRD) = 20;
+			break;
+		
+		case BAUD_57600:
+			accessReg(UART0IBRD) = 130;
+			accessReg(UART0FBRD) = 13;
+			break;
+		
+		case BAUD_115200:
+			accessReg(UART0IBRD) = 65;
+			accessReg(UART0FBRD) = 7;
+			break;
+		default: 
+			accessReg(UART0IBRD) = 65;
+			accessReg(UART0FBRD) = 7;
+			break;
+	}
+	
+	accessReg(UART0LCRH) = dataConfig; // Configura os parametros da serial
+	accessReg(UART0CC) = 0x0; // Configura para usar o clock normal do sistema (igual a reset)
+	
+	accessReg(UART0CTL) |= (1<<0) | (1<<8) | (1<<9); // Reabilita o UART
+}
+
+void uart_putChar(char c) {
+	while(accessReg(UART0FR) & (1<<5)); // Espera a FIFO de saida estar vazia
+	accessReg(UART0DR) = c; // Coloca valor no reg de dados
+}
+
+char uart_getChar() {
+	char c;
+	while(accessReg(UART0FR) & (1<<4)); // Espera a FIFO de entrada estar vazia
+	c = accessReg(UART0DR); // Pega o valor do reg de dados. Variavel intermediaria por seguranca
+	return(c);
+}
---
+++ lab04/uart_lab4.h
@@ -0,0 +1,78 @@
+/**
+* Mini biblioteca para uso de UART
+* UART em uso eh o UART0
+* Documentacao e passos encontrados em http://www.ti.com/lit/ds/symlink/tm4c1294ncpdt.pdf
+* Ver secao "16.4 Initialization and Configuration"
+*/
+
+// DELETAR DEPOIS
+// VIDEO REFERENCIA
+// https://www.youtube.com/watch?v=Vy-k7u5djM4&t=20s
+
+#ifndef __UART_LAB4_H__
+#define __UART_LAB4_H__
+
+#include <stdint.h>
+
+// Para facilidade de uso dos registradores
+#define accessReg(x) \
+        (*((volatile uint32_t *)(x)))
+
+// Registradores de configuracao
+#define RCGCUART   0x400FE618 // Registrador de controle dos modulos UART
+#define RCGCGPIO   0x400FE608 // Registrador de controle de clock de modulos de I/O
+#define GPIOAFSELA 0x40058420 // Registrador que controla se pinos são I/O generico ou usados por perifericos para porta A
+#define GPIOPCTLA  0x4005852C // Registrador usado em conjunto com GPIOAFSELA para selecionar o tipo de sinal para cada pino
+#define GPIOADEN   0x4005851C // Registrador que habilita e desabilita sinais digitais
+#define UART0CTL   0x4000C030 // Registrador de controle da UART
+#define UART0IBRD  0x4000C024 // Registrador que guarda o valor inteiro do divisor de baudrate
+#define UART0FBRD  0x4000C028 // Registrador que guarda o valor de fracao do divisor de baudrate
+#define UART0LCRH  0x4000C02C // Registrador de configuracao da serial (data bits, paridade, stop bit, etc)
+#define UART0CC    0x4000CFC8 // Registrador de controle de clock source da UART
+
+// Registradores de flag e transmissao
+#define UART0FR    0x4000C018 // Registrador de flags
+#define UART0DR    0x4000C000 // Registrador de dados
+
+/* Funcao de inicializacao. Deve ser chamada antes de tentar enviar ou receber
+   Notar os defines abaixo a serem usados com a funcao */
+/* Usar os defines de como o seguinte exemplo:
+	uart_init(BAUD_9600, DATA_8 | STOP_ONE | PARITY_NONE);
+	O init acima configura baudrate em 9600, 8 databits, 1 stop bit e sem paridade */
+void uart_init(uint32_t baudRate, uint32_t dataConfig);
+/* A baixo, defines que devem ser usados com a funcao de init */
+// Defines baudrate
+#define BAUD_75 0
+#define BAUD_110 1
+#define BAUD_300 2
+#define BAUD_1200 3
+#define BAUD_2400 4
+#define BAUD_4800 5
+#define BAUD_9600 6
+#define BAUD_19200 7
+#define BAUD_38400 8
+#define BAUD_57600 9
+#define BAUD_115200 10
+
+// Defines data bits
+#define DATA_5 0x0
+#define DATA_6 0x20
+#define DATA_7 0x40
+#define DATA_8 0x60
+
+// Defines stop bits
+#define STOP_ONE 0x0
+#define STOP_TWO 0x8
+
+// Defines paridade
+#define PARITY_NONE 0x0
+#define PARITY_ODD  0x2
+#define PARITY_EVEN 0x6
+
+// Funcao para enviar caracteres BLOQUEANTE
+void uart_putChar(char c);
+
+// Funcao para receber caracteres BLOQUEANTE
+char uart_getChar();
+
+#endif
