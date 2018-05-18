7dc0c56523e67c26f3d84a8fec3b7b10eb65f5d9
Angelin01 <angeloborsoiross@hotmail.com>
2018-05-17 21:50:02 -0300

    Eu me toquei q ter o \0 eh idiota

--- lab04/colors.h
+++ lab04/colors.h
@@ -18,13 +18,13 @@ typedef struct ColorGroup {
 // Blue Colors
 const ColorGroup blueColors1 = {
 	{ClrSlateBlue, ClrDarkSlateBlue, ClrMidnightBlue, ClrNavy, ClrDarkBlue, ClrMediumBlue, ClrBlue, ClrCornflowerBlue, ClrRoyalBlue, ClrDodgerBlue},
-	{"Slate Blue\0", "Dark Slate Blue\0", "Midnight Blue\0", "Navy\0", "Dark Blue\0", "Medium Blue\0", "Blue\0", "Cornflower Blue\0", "Royal Blue\0", "Dodger Blue\0"},
+	{"Slate Blue", "Dark Slate Blue", "Midnight Blue", "Navy", "Dark Blue", "Medium Blue", "Blue", "Cornflower Blue", "Royal Blue", "Dodger Blue"},
 	10,
 	"Blue 1"
 };
 const ColorGroup blueColors2 = {
 	{ClrDeepSkyBlue, ClrLightSkyBlue, ClrSkyBlue, ClrLightBlue, ClrSteelBlue, ClrLightSteelBlue, ClrSlateGray, ClrLightSlateGray},
-	{"Deep Sky Blue\0", "Light Sky Blue\0", "Sky Blue\0", "Light Blue\0", "Steel Blue\0", "Light Steel Blue\0", "Slate Gray\0", "Light Slate Gray\0"},
+	{"Deep Sky Blue", "Light Sky Blue", "Sky Blue", "Light Blue", "Steel Blue", "Light Steel Blue", "Slate Gray", "Light Slate Gray"},
 	8,
 	"Blue 2"
 };
@@ -32,13 +32,13 @@ const ColorGroup blueColors2 = {
 // Green Colors
 const ColorGroup greenColors1 = {
 	{ClrDarkSlateGray, ClrMediumSpringGreen, ClrSpringGreen, ClrPaleGreen, ClrLightGreen, ClrDarkSeaGreen, ClrMediumSeaGreen, ClrSeaGreen, ClrDarkGreen, ClrGreen},
-	{"Dark Slate Gray\0", "Medium Spring Green\0", "Spring Green\0", "Pale Green\0", "Light Green\0", "Dark Sea Green\0", "Medium Sea Green\0", "Sea Green\0", "Dark Green\0", "Green\0"},
+	{"Dark Slate Gray", "Medium Spring Green", "Spring Green", "Pale Green", "Light Green", "Dark Sea Green", "Medium Sea Green", "Sea Green", "Dark Green", "Green"},
 	10,
 	"Green 1"
 };
 const ColorGroup greenColors2 = {
 	{ClrForestGreen, ClrLimeGreen, ClrLime, ClrLawnGreen, ClrChartreuse, ClrGreenYellow, ClrYellowGreen, ClrOliveDrab, ClrDarkOliveGreen, ClrOlive},
-	{"Forest Green\0", "Lime Green\0", "Lime\0", "Lawn Green\0", "Chartreuse\0", "Green Yellow\0", "Yellow Green\0", "Olive Drab\0", "Dark Olive Green\0", "Olive\0"},
+	{"Forest Green", "Lime Green", "Lime", "Lawn Green", "Chartreuse", "Green Yellow", "Yellow Green", "Olive Drab", "Dark Olive Green", "Olive"},
 	10,
 	"Green 2"
 };
@@ -46,13 +46,13 @@ const ColorGroup greenColors2 = {
 // Brown Colors
 const ColorGroup brownColors1 = {
 	{ClrDarkKhaki, ClrGoldenrod, ClrDarkGoldenrod, ClrSaddleBrown, ClrSienna, ClrRosyBrown, ClrPeru, ClrChocolate, ClrSandyBrown, ClrNavajoWhite},
-	{"Dark Khaki\0", "Goldenrod\0", "Dark Goldenrod\0", "Saddle Brown\0", "Sienna\0", "Rosy Brown\0", "Peru\0", "Chocolate\0", "Sandy Brown\0", "Navajo White\0"},
+	{"Dark Khaki", "Goldenrod", "Dark Goldenrod", "Saddle Brown", "Sienna", "Rosy Brown", "Peru", "Chocolate", "Sandy Brown", "Navajo White"},
 	10,
 	"Brown 1"
 };
 const ColorGroup brownColors2 = {
 	{ClrWheat, ClrBurlyWood, ClrTan},
-	{"Wheat\0", "Burly Wood\0", "Tan\0"},
+	{"Wheat", "Burly Wood", "Tan"},
 	3,
 	"Brown 2"
 };
@@ -60,13 +60,13 @@ const ColorGroup brownColors2 = {
 // Purple Colors
 const ColorGroup purpleColors1 = {
 	{ClrMediumSlateBlue, ClrMediumPurple, ClrBlueViolet, ClrIndigo, ClrDarkViolet, ClrDarkOrchid, ClrMediumOrchid, ClrPurple, ClrDarkMagenta, ClrMagenta},
-	{"Medium Slate Blue\0", "Medium Purple\0", "Blue Violet\0", "Indigo\0", "Dark Violet\0", "Dark Orchid\0", "Medium Orchid\0", "Purple\0", "Dark Magenta\0", "Magenta\0"},
+	{"Medium Slate Blue", "Medium Purple", "Blue Violet", "Indigo", "Dark Violet", "Dark Orchid", "Medium Orchid", "Purple", "Dark Magenta", "Magenta"},
 	10,
 	"Purple 1"
 };
 const ColorGroup purpleColors2 = {
 	{ClrViolet, ClrOrchid, ClrPlum},
-	{"Violet\0", "Orchid\0", "Plum\0"},
+	{"Violet", "Orchid", "Plum"},
 	3,
 	"Purple 2"
 };
@@ -74,7 +74,7 @@ const ColorGroup purpleColors2 = {
 // Cyan Colors
 const ColorGroup cyanColors = {
 	{ClrCyan, ClrDarkTurquoise, ClrTurquoise, ClrMediumTurquoise, ClrLightSeaGreen, ClrDarkCyan, ClrTeal, ClrAquamarine, ClrMediumAquamarine, ClrCadetBlue},
-	{"Cyan\0", "Dark Turquoise\0", "Turquoise\0", "Medium Turquoise\0", "Light Sea Green\0", "Dark Cyan\0", "Teal\0", "Aquamarine\0", "Medium Aquamarine\0", "Cadet Blue\0"},
+	{"Cyan", "Dark Turquoise", "Turquoise", "Medium Turquoise", "Light Sea Green", "Dark Cyan", "Teal", "Aquamarine", "Medium Aquamarine", "Cadet Blue"},
 	10,
 	"Cyan Colors"
 };
@@ -82,7 +82,7 @@ const ColorGroup cyanColors = {
 // Orange Colors
 const ColorGroup orangeColors = {
 	{ClrOrangeRed, ClrDarkOrange, ClrOrange},
-	{"Orange Red\0", "Dark Orange\0", "Orange\0"},
+	{"Orange Red", "Dark Orange", "Orange"},
 	3,
 	"Orange Colors"
 };
@@ -90,7 +90,7 @@ const ColorGroup orangeColors = {
 // Yellow Colors
 const ColorGroup yellowColors = {
 	{ClrGold, ClrYellow, ClrKhaki},
-	{"Gold\0", "Yellow\0", "Khaki\0"},
+	{"Gold", "Yellow", "Khaki"},
 	3,
 	"Yellow Colors"
 };
@@ -98,7 +98,7 @@ const ColorGroup yellowColors = {
 // Pink Colors
 const ColorGroup pinkColors = {
 	{ClrMediumVioletRed, ClrDeepPink, ClrHotPink, ClrPaleVioletRed, ClrLightPink, ClrPink, ClrLightCoral, ClrIndianRed, ClrCrimson},
-	{"Medium Violet Red\0", "Deep Pink\0", "Hot Pink\0", "Pale Violet Red\0", "Light Pink\0", "Pink\0", "Light Coral\0", "Indian Red\0", "Crimson\0"},
+	{"Medium Violet Red", "Deep Pink", "Hot Pink", "Pale Violet Red", "Light Pink", "Pink", "Light Coral", "Indian Red", "Crimson"},
 	9,
 	"Pink Colors"
 };
@@ -106,7 +106,7 @@ const ColorGroup pinkColors = {
 // Red Colors
 const ColorGroup redColors = {
 	{ClrMaroon, ClrDarkRed, ClrFireBrick, ClrBrown, ClrSalmon, ClrDarkSalmon, ClrLightSalmon, ClrCoral, ClrTomato, ClrRed},
-	{"Maroon\0", "Dark Red\0", "Fire Brick\0", "Brown\0", "Salmon\0", "Dark Salmon\0", "Light Salmon\0", "Coral\0", "Tomato\0", "Red\0"},
+	{"Maroon", "Dark Red", "Fire Brick", "Brown", "Salmon", "Dark Salmon", "Light Salmon", "Coral", "Tomato", "Red"},
 	10,
 	"Red Colors"
 };
